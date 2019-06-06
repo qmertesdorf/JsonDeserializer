84508428c8aa50365ab4f150a4bb748df8fc3df2
qmertesdorf <quintenmertesdorf@gmail.com>
2019-06-06 12:25:47 -0700

    initial commit; add initial console command reading/writing

---
+++ JsonDeserializer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace JsonDeserializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string argument = args[0];
+            string filepath = args[1];
+            if (argument == "-f")
+            { 
+                Console.WriteLine("Argument is: " + argument);
+                Console.WriteLine("Filepath is: " + filepath);
+            }
+        }
+    }
+}
