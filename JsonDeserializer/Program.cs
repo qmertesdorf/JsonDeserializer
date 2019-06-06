b1df66e3f6204cb67f333af28b9e21bc92e4ceaa
qmertesdorf <quintenmertesdorf@gmail.com>
2019-06-06 15:21:23 -0700

    add appsettings configuration; handle error cases for invalid files

--- JsonDeserializer/Program.cs
+++ JsonDeserializer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Configuration;
 
 namespace JsonDeserializer
 {
@@ -8,15 +9,29 @@ namespace JsonDeserializer
     {
         static void Main(string[] args)
         {
-            string argument = args[0];
-            string filepath = args[1];
+            //Read data from appsettings.json
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
 
-            if (argument != "-f" || !File.Exists(filepath))
+            string filepath = null;
+            if (config["filepath"] != null && File.Exists(config["filepath"]))
             {
-                //In the case where user did not pass in the "-f" argument or the entered filepath does not exist, exit application
+                filepath = config["filepath"];
+            }
+            else if (args.Length == 2 && File.Exists(args[1]) && args[0] == "-f")
+            {
+                filepath = args[1];
+            } else
+            {
+                //In the case where user did not pass in the "-f" argument or there is no valid filepath, exit application
                 //User-facing error message would go here
                 Environment.Exit(1);
             }
+
             JObject parsedJSON = JObject.Parse(File.ReadAllText(@filepath));
             JArray jsonArray = (JArray)parsedJSON["items"];
             foreach (JToken item in jsonArray)
